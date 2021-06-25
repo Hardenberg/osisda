e8e7b6d8b1425f9b5be230b3dcd40764ee50cb6d
Alrik Schnapke <alrik.schnapke+GitHub@gmail.com>
2021-06-25 06:40:48 +0200

    small stuff

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,23 +1,23 @@
 import express from "express";
 import cors from "cors";
 
-export interface OsisdaServerInterface{
+export interface OsisdaServerInterface {
     start(): void;
 }
 
-export class OsisdaServer implements OsisdaServerInterface{
+export class OsisdaServer implements OsisdaServerInterface {
     start(): void {
         const server = express();
         server.use(cors())
 
-        server.get( "/", ( _req, res ) => {
+        server.get("/", (_req, res) => {
             res.send('hello World');
         });
         server.use('/login', (_req, res) => {
             res.send({
-              token: 'test123'
+                token: 'test123'
             });
-          });
+        });
 
         server.listen(3001, () => {
             console.log('is running')
